c6b8a24fa1e5da77553ee5742f9f977032c37731
yunhaooo <liyunhaoLearn@gmail.com>
2018-03-24 18:22:15 -0400

    listener class header file

---
+++ include/Listener.hpp
@@ -0,0 +1,24 @@
+#include "RUDP.hpp"
+#ifndef Listener_hpp
+#define Listener_hpp
+
+
+class Listener{
+public:
+	int duplicateACK = 0;
+	unsigned int ACKcwnd = 0;
+	ms eRTT;
+	ms sRTT;
+	ms deviation;
+	static const int control=8;
+
+	void recAns();
+	void update(unsigned int sendbase);
+	void fastRecovery();
+	void index(unsigned int sendbase);
+	void linear(unsigned int sendbase);
+	void getTimeout();	
+};
+
+#endif
+	 
